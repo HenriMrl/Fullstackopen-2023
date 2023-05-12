a3a13b47db1d6a5d4f5d5a882691b60234636b33
Henri Meriläinen <henri.merilainen1@gmail.com>
2023-05-12 14:33:47 +0300

    tehtävät 1-1.2 palautus

---
+++ osa1-tehtävät/tehtävät 1-1.14/src/App.js
@@ -0,0 +1,63 @@
+
+const Header = (props) => { 
+  return (
+    <div>
+      <p>
+      the course is: {props.course}
+      </p>
+    </div>
+  )
+}
+
+const Part = (props) => { 
+  return (
+    <div>
+      <p>
+      the name of the course: {props.part} 
+      <br></br> 
+      the number of excercises for the part: {props.exercises} exercises.
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => { 
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>  
+    </div>
+  )
+}
+
+
+const Total = (props) => { 
+  return (
+    <div>
+      <p>
+      the total number of exercises is : {props.total}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  ) 
+}
+
+export default App
