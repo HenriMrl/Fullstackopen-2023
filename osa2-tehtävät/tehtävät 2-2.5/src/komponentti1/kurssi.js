c895efc661e669f67997530811cdba24bece38ce
Henri Meriläinen <henri.merilainen1@gmail.com>
2023-06-05 15:34:49 +0300

    tehtävien 2-2.5 palautus

---
+++ osa2-tehtävät/tehtävät 2-2.5/src/komponentti1/kurssi.js
@@ -0,0 +1,17 @@
+const Course = (props) => {
+    return (
+      <div>
+        <h1>Web development curriculum</h1>
+        <ul>
+          {props.course.map((course) =><li key={course.id}> <h2>{course.name}</h2>
+          <ul>
+            {course.parts.map((part) =><li key={part.id}> {part.name} {part.exercises}</li>)}
+          </ul>
+          <p>total amount of exercises: {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+          </li>)}
+        </ul>
+      </div>
+    );
+  };
+
+export default Course
